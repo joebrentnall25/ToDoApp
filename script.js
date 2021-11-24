463824a9bed17c6e476a41330faccae34d77e870
Joseph Brentnall <18679097@students.lincoln.ac.uk>
2021-11-24 16:16:20 +0000

    Added a counter to show the amount of tasks to complete

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const tasks = [];
 
-const inputText = document.getElementById('form__input')
+const inputText = document.getElementById('form__input');
 const addTaskBtn = document.getElementById('form__btn');
-const tasksContainer = document.getElementById('tasks__container')
+const tasksContainer = document.getElementById('tasks__container');
+const tasksCounter = document.getElementById('tasks__counter');
 
 addTaskBtn.addEventListener('click', () => {
     const task = { 
@@ -26,4 +27,18 @@ const addToScreen = (task) => {
             <i class="fas fa-pen tasks__edit"></i>
         </div>`;          
     tasksContainer.innerHTML+=html;
+    updateTaskCounter();
 }
+
+const updateTaskCounter = () => {
+    if (tasks.length != 0){
+        let html = `<p class="para-font">Tasks to Complete: ${tasks.length}</p>`
+        console.log(html)
+        tasksCounter.innerHTML = html;
+    } else {
+        let html = "<p class='para-font'>All tasks have been completed. Great Job!</p>"
+        tasksCounter.innerHTML = html;
+    }
+}
+
+window.onload = updateTaskCounter;
